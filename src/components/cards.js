d28ff7921c367b5ffa85961175803fa5fe90979c
lorenzosegada <lorenzosegada@gmail.com>
2025-06-11 21:10:10 -0300

    Ajuste de tarjetas, cambio en la  la alineación del botón

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,10 +2,10 @@ export function Card(productId, imgUrl, alt, cardTitle) {
     return `
     <div class="col">
         <div class="card card-hover h-100 d-flex flex-column">
-            <img src="${imgUrl}" class="card-img-top mt-2" alt="${alt}" style="height: 250px; object-fit: contain;">
+            <img src="${imgUrl}" class="card-img-top mt-2" alt="${alt}" style="height: 200px; object-fit: contain;">
             <div class="card-body d-flex flex-column">
-            <h5 class="card-title text-center">${cardTitle}</h5>
-            <div class="text-center"> 
+            <p class="card-title text-center mb-3">${cardTitle}</p>
+            <div class="text-center mt-auto"> 
                 <button class="btn btn-outline-dark detalle-btn" data-id="${productId}" id="btn-product-${productId}">Más detalles</button>
             </div>
         </div>
